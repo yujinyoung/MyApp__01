ace907d001b4343695aa2fb22ec3182b8c453370
Jinyoung Yu <appcreator08@AppCreator08ui-iMac.local>
2016-06-16 20:11:40 +0900

    #2 First and add

--- app/src/main/java/kr/hs/emirim/dbwlsdud0407/myapp/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dbwlsdud0407/myapp/MainActivity.java
@@ -10,17 +10,30 @@ import android.widget.ViewFlipper;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     ViewFlipper flip;
     EditText editTime;
+    int time;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         flip=(ViewFlipper)findViewById(R.id.but_flip);
+        editTime=(EditText)findViewById(R.id.edit_time)
         Button butstr=(Button)findViewById(R.id.but_start);
         butstr.setOnClickListener(this);
         Button butstop=(Button)findViewById(R.id.but_stop);
         butstop.setOnClickListener(this);
     }
     public void onClick(View v){
+        switch(v.getId()){
+            case R.id.but_start:
+                time=(int)(Double.parseDouble(editTime.getText().toString())*1000);
+                flip.setFlipInterval(time);
+                flip.startFlipping();
+                break;
+            case R.id.but_stop:
+                flip.stopFlipping();
+                break;
 
+        }
+    }
     }
 }
