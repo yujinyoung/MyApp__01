6ff42c3e7e10b003365f6e20780ddbf9df70ce35
Jinyoung Yu <appcreator08@AppCreator08ui-iMac.local>
2016-06-16 19:58:29 +0900

    #1 first

--- app/src/main/java/kr/hs/emirim/dbwlsdud0407/myapp/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dbwlsdud0407/myapp/MainActivity.java
@@ -2,12 +2,25 @@ package kr.hs.emirim.dbwlsdud0407.myapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ViewFlipper;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    ViewFlipper flip;
+    EditText editTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        flip=(ViewFlipper)findViewById(R.id.but_flip);
+        Button butstr=(Button)findViewById(R.id.but_start);
+        butstr.setOnClickListener(this);
+        Button butstop=(Button)findViewById(R.id.but_stop);
+        butstop.setOnClickListener(this);
+    }
+    public void onClick(View v){
+
     }
 }
